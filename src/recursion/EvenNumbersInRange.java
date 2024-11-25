c8fe8e0c553d9d574d2338cad8674b2e61ec70e6
javapotato24 <evan.wu25@ycdsbk12.ca>
2024-11-25 11:00:17 -0500

    finished print evens

---
+++ src/recursion/EvenNumbersInRange.java
@@ -0,0 +1,16 @@
+package recursion;
+
+public class EvenNumbersInRange {
+    public static void printEvens(int start, int end) {
+        if(start <= end){
+            if(start %2 == 0){
+                System.out.println(start);
+            }
+            printEvens(start + 1 ,end);
+        }
+    }
+
+    public static void main(String[] args) {
+        printEvens(1, 5);
+    }
+}

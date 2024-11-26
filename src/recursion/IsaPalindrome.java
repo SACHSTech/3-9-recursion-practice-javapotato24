2295b6682e17eecb433f24e9e97bfe4f20dcf172
javapotato24 <evan.wu25@ycdsbk12.ca>
2024-11-26 10:38:42 -0500

    finished palindrome

---
+++ src/recursion/IsaPalindrome.java
@@ -0,0 +1,20 @@
+package recursion;
+
+public class IsaPalindrome {
+    public static boolean isPalindrome(String s){
+
+        if(s.length() == 1){
+           return true;
+        }else if(s.substring(0, 1).equals(s.substring(s.length() - 1 , s.length()))){
+            return isPalindrome(s.substring(s.length() - (s.length() - 1), s.length() - 1));
+        }else{
+            return false;
+        }
+    }
+        
+    
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("mom"));
+    }
+}
